39d9782dc4cf930d8beb575dbf2c834d13510c3b
carlos <c.delriofrances@gmail.com>
2023-11-16 14:18:47 -0300

    updated tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  purge: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
     extend: {},
