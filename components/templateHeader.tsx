fe7ba4a39d6ed4ede2beaa0ff3ff7d60222edfe5
carlos <c.delriofrances@gmail.com>
2023-11-20 16:28:27 -0300

    mobil fix

--- components/templateHeader.tsx
+++ components/templateHeader.tsx
@@ -14,7 +14,7 @@ const TemplateHeader: React.FC<TemplateHeaderProps> = ({
   icon,
 }) => {
   return (
-    <div className="sticky top-0 flex items-center px-6 py-4 bg-white border-b border-gray-200 z-10">
+    <div className="z-0 sticky top-0 flex items-center px-6 py-4 bg-white border-b border-gray-200 z-10">
       <div className="mr-6">
         <div className="flex items-center justify-center w-10 h-10 text-gray-500">
           <div className="flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-xl ring-2 ring-offset-2 ring-gray-100 group-hover:ring-gray-200 group-focus:ring-gray-300 group-hover:bg-white">
